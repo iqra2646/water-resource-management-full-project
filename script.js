e08c7752524d8f460e1c71a42e261d0dd8c84b6f
iqra <iqralul9@gmail.com>
2024-06-22 20:11:34 +0300

    done project of phase1

--- script.js
+++ script.js
@@ -1,23 +1,78 @@
-function finalProject(project){
-    const row = document.getElementById("card")
-    const div = document.createElement("div")
+document.querySelector("form").addEventListener("submit",prjects);
+function prjects(e){ 
+e.preventDefault()
+const Title=document.getElementById("title")
+const Plot=document.getElementById("plot")
+const Poster=document.getElementById("poster")
+
+
+
+const Objectproject={ 
+
+    Title: Title.value,
+    Plot: Plot.value,
+    Poster:Poster.value,
+    }
+    
+    fetch("http://localhost:3000/projects",{ 
+    method:"POST",
+    header:{ 
+    'content-type':'application/json'
+    },
+    body:JSON.stringify(Objectproject)
+    })
+    .then(res=>res.json())
+    .then(console.log("submitted"))
+    
+    
+    }
+    
+
+function addproject(project){
+    let row =document.getElementById("card")
+    let div =document.createElement("div")
     div.classList.add("col-3")
-    div.innerHTML = `<div class="card">
-    <img src="${project.image}" class="card-img-top" alt="...">
+    div.innerHTML=`<div class="card">
+    <img src="${project.Poster}" class="card-img-top" alt="" height="300px">
     <div class="card-body">
-      <h5 class="card-title">${project.name}</h5>
-      <p class="card-text">${project.description}</p>
-      <a href="https://www.pexels.com/photo/sunlit-beams-over-borehole-16227435/" class="btn btn-info">see more</a>
+      <h5 class="card-title">${project.Title}</h5>
+      <p class="card-text">${project.Plot}</p>
+      <button><a href="#" class="btn btn-outline">Delete</a></button>
     </div>
   </div>`
   row.appendChild(div)
-}
-function projectData(project){
-    fetch("http://localhost:3000/projects")
-    .then(res =>res.json())
-    .then(project =>{
-        project.forEach(finalProject)
-    })
+
+
+ // 6 deleting update but not json server
+ div.querySelector("button").addEventListener("click" , function(){
+    div.remove()
+   deleteproject(project.id)
+  })
 }
 
-projectData()
+
+// 7 deleting update also from json server
+function deleteproject(id){
+  fetch("http://localhost:3000/update/${id}",{
+    method: "DELETE",
+    headers: {
+      'content-type':'application/json'
+    }
+  })
+}
+  
+
+
+  function getproject(project){
+    fetch("http://localhost:3000/projects")
+    .then(res=>res.json())
+    .then(project =>{
+     project.forEach(addproject)
+     
+    })
+    }
+
+
+    document.addEventListener("DOMContentLoaded", function(){
+        getproject();
+      })
